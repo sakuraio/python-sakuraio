a598f31ceaee3f62d7929ec45fc2623468799d71
whywaita <whywaita@whywrite.it>
2017-07-01 02:44:08 +0900

    add software module (only GET)

---
+++ sakuraio/software/base.py
@@ -0,0 +1,33 @@
+import sys
+import urllib2
+import urlparse
+
+
+class SakuraIOClient(object):
+    base_url = 'https://api.sakura.io/'
+    api_token = None
+    api_secret = None
+
+    def __init__(self, api_token, api_secret, base_url=None):
+        if api_token is None or api_secret is None:
+            sys.stderr.write('[ERROR] must set api_token and api_secret')
+
+        self.api_token = api_token
+        self.api_secret = api_secret
+
+        if base_url is not None:
+            self.base_url = base_url
+
+    def do(self, method, url, url_params={}, query_params={}, request_params={}):
+        headers = {}
+        headers["authorization"] = "Basic " + (self.api_token + ":" + self.api_secret).encode("base64")[:-1]
+        headers["Accept"] = 'application/json'
+        url = url.lstrip('/')
+        _url = self.base_url + url
+
+        method = method.lower()
+        response = None
+        if method == 'get':
+            response = urllib2.request.urlopen(_url, params=query_params)
+
+        return response.json()
